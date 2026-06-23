dabec5c28919c5e665fadf54b62e43542110fa87
suzikang17 <suzikang17@gmail.com>
2026-06-22 20:30:25 -0500

    changes: ChangedFile/GitCommit/FileDiffSource models

---
+++ Sources/DevDash/Models/DiffModels.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct ChangedFile: Identifiable, Hashable {
+    let path: String            // repo-relative
+    let stagedStatus: Character?    // index (X) column; nil if unmodified there
+    let unstagedStatus: Character?  // worktree (Y) column; nil if unmodified there
+    let isUntracked: Bool
+    var id: String { path }
+    var name: String { (path as NSString).lastPathComponent }
+}
+
+struct GitCommit: Identifiable, Hashable {
+    let sha: String
+    let shortSha: String
+    let subject: String
+    let author: String
+    let relativeDate: String
+    var id: String { sha }
+}
+
+enum FileDiffSource: Hashable {
+    case unstaged
+    case staged
+    case commit(String)
+}
